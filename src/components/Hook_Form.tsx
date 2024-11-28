577908023426d048476f61f92e7a1886ee4b99f4
Guilhermefariah <Guilhermefaria321@gmail.com>
2024-11-27 22:28:22 -0300

    feat: adicionado hook form para gerenciamento de formulário

--- src/components/Hook_Form.tsx
+++ src/components/Hook_Form.tsx
@@ -1,48 +1,49 @@
 "use client"
-import { FormEvent, useState } from "react"
+
+import { useForm } from "react-hook-form"
+
+type TFormData = {
+  name: string
+  age: number
+}
 
 export function HookForm() {
-  const [name, setName] = useState("")
-  const [age, setAge] = useState("")
+  const { register, handleSubmit } = useForm<TFormData>()
 
-  const handleSave = (e: FormEvent) => {
-    e.preventDefault()
-
-    console.log({ name, age })
+  const handleSave = (data: TFormData) => {
+    console.log(data)
   }
 
   return (
     <>
       <main className="flex justify-center items-center min-h-screen bg-gray-800">
         <form
-          onSubmit={handleSave}
-          className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md"
+          onSubmit={handleSubmit(handleSave, (errors) => console.log(errors))}
+          className="bg-white text-gray-800 p-8 rounded-lg shadow-lg w-full max-w-md"
         >
           <fieldset>
-            <legend className="text-xl font-semibold text-gray-400 text-center mb-4">
+            <legend className="text-xl font-semibold text-gray-800 text-center mb-4">
               Form
             </legend>
 
             <label className="block mb-4">
-              <span className="block text-gray-400 font-semibold">Name</span>
+              <span className="block text-gray-800 font-semibold">Name</span>
               <input
                 type="text"
-                value={name}
                 placeholder="Enter your name"
-                onChange={(e) => setName(e.target.value)}
                 className="w-full p-3 mt-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                {...register("name")}
               />
               <span className="text-sm text-gray-500">Type your name here</span>
             </label>
 
             <label className="block mb-4">
-              <span className="block text-gray-400 font-semibold">Age</span>
+              <span className="block text-gray-800 font-semibold">Age</span>
               <input
                 type="number"
-                value={age}
                 placeholder="Enter your age"
-                onChange={(e) => setAge(e.target.value)}
                 className="w-full p-3 mt-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                {...register("age")}
               />
               <span className="text-sm text-gray-500">Type your age here</span>
             </label>
